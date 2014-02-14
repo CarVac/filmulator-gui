635324a6efcb241fbb7f68556c7cddb321450267
CarVac <c.lo.to.da.down.lo@gmail.com>
2014-02-14 12:00:59 -0500

    Added a todo: a list of functions that need replacing to get sqlmodel to work with Qt Quick views

--- Filmulator-Testing-2/database/sqlmodel.h
+++ Filmulator-Testing-2/database/sqlmodel.h
@@ -5,6 +5,18 @@
 #include <QDir>
 #include <QFile>
 
+//TODO: We need to replace
+//a) flags()
+//b) setData()
+//c) insertRows()
+//d) removeRows()
+
+//Look at "Using C++ Models with Qt Quick Views to figure out what else we need.
+//It seems that by subclassing QSqlQueryModel we may not need to notify when doing queries...
+
+//Also, we need to replace the convenience functions so that we can more easily set up
+//queries. If possible.
+
 class SqlModel : public QSqlQueryModel//QSqlRelationalTableModel
 {
     Q_OBJECT
