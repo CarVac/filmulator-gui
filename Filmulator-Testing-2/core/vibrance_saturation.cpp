48b21e7b6ca1e4acd662f35b078da8a335f90531
Omer Mano <mermerico@gmail.com>
2014-03-02 22:13:54 -0500

    Changed saturation/vibrance from a linear function on S to a exponential function

--- Filmulator-Testing-2/core/vibrance_saturation.cpp
+++ Filmulator-Testing-2/core/vibrance_saturation.cpp
@@ -90,15 +90,56 @@ void HSVtoRGB( float h, float s, float v, float &r, float &g, float &b)
 
 }
 
+float log2(float i)
+{
+    return(log(i)/log(2));
+}
+float shift23=(1<<23);
+float OOshift23=1.0/(1<<23);
+
+float myLog2(float i)
+{
+    float LogBodge=0.346607f;
+    float x;
+    float y;
+    x=*(int *)&i;
+    x*= OOshift23; //1/pow(2,23);
+    x=x-127;
+
+    y=x-floorf(x);
+    y=(y-y*y)*LogBodge;
+    return x+y;
+}
+float myPow2(float i)
+{
+    float PowBodge=0.33971f;
+    float x;
+    float y=i-floorf(i);
+    y=(y-y*y)*PowBodge;
+
+    x=i+127-y;
+    x*= shift23; //pow(2,23);
+    *(int*)&x=(int)x;
+    return x;
+}
+
+float myPow(float a, float b)
+{
+    return myPow2(b*myLog2(a));
+}
+
 void vibrance_saturation(matrix<unsigned short> &input,
                          matrix<unsigned short> &output,
                          double vibrance, double saturation)
 {
-    double yIntercept = vibrance + saturation;
-    double slope = 1-vibrance;
     int nrows = input.nr();
     int ncols = input.nc();
+    double gamma = pow(2,-vibrance);
+    double sat = pow(2,saturation);
     output.set_size(nrows,ncols);
+#pragma omp parallel shared(output, input) firstprivate(nrows,ncols,saturation,gamma)
+    {
+#pragma omp for schedule(dynamic) nowait
     for(int i = 0; i < nrows; i++)
         for(int j = 0; j < ncols; j += 3)
         {
@@ -107,10 +148,11 @@ void vibrance_saturation(matrix<unsigned short> &input,
             float b = float(input(i,j+2))/65535.0;
             float h,s,v;
             RGBtoHSV(r,g,b,h,s,v);
-            s = max(min( yIntercept + slope*s, 1.0),0.0);
+            s = max(min( sat*myPow(s,gamma), 1.0),0.0);
             HSVtoRGB(h,s,v,r,g,b);
             output(i,j  ) = r*65535;
             output(i,j+1) = g*65535;
             output(i,j+2) = b*65535;
         }
+    }
 }
