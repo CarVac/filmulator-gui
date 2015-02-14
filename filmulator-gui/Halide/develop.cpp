fc4f8ddf6fafc77ebbaa70e97c58dd00d6c0bd36
Omer Mano <mermerico@gmail.com>
2015-02-14 03:57:15 -0500

    Turned develop Exprs int Funcs in an effort to save computation, No gains found but committing anyways for future reference

--- filmulator-gui/Halide/develop.cpp
+++ filmulator-gui/Halide/develop.cpp
@@ -20,32 +20,39 @@ Func develop(Func inputs, Expr crystalGrowthConst,
   Expr dcc = 2.0f*developerConsumptionConst / ( activeLayerThickness*3.0f);
   Expr sscc = silverSaltConsumptionConst * 2.0f;
 
-  Expr dCrystalRadR = inputs(x,y,DEVEL_CONC) * inputs(x,y,SILVER_SALT_DEN_R) * cgc;
-  Expr dCrystalRadG = inputs(x,y,DEVEL_CONC) * inputs(x,y,SILVER_SALT_DEN_G) * cgc;
-  Expr dCrystalRadB = inputs(x,y,DEVEL_CONC) * inputs(x,y,SILVER_SALT_DEN_B) * cgc;
+  Func dCrystalRad;
+  //                                            Silver Salt Density
+  dCrystalRad(x,y,c) = inputs(x,y,DEVEL_CONC) * inputs(x,y,c+6) * cgc;
 
-  Expr dCrystalVolR = dCrystalRadR * inputs(x,y,CRYSTAL_RAD_R) * inputs(x,y,CRYSTAL_RAD_R) *
-                      inputs(x,y,ACTIVE_CRYSTALS_R);
-  Expr dCrystalVolG = dCrystalRadG * inputs(x,y,CRYSTAL_RAD_G) * inputs(x,y,CRYSTAL_RAD_G) *
-                      inputs(x,y,ACTIVE_CRYSTALS_G);
-  Expr dCrystalVolB = dCrystalRadB * inputs(x,y,CRYSTAL_RAD_B) * inputs(x,y,CRYSTAL_RAD_B) *
-                      inputs(x,y,ACTIVE_CRYSTALS_B);
+  Func dCrystalVol;
+  //                                        Crystal Radius                  Active Crystals
+  dCrystalVol(x,y,c) = dCrystalRad(x,y,c) * inputs(x,y,c) * inputs(x,y,c) * inputs(x,y,c+3);
 
   Func outputs;
   outputs(x,y,c) = select(
-             c == CRYSTAL_RAD_R    , inputs(x,y,c) + dCrystalRadR,
-      select(c == CRYSTAL_RAD_G    , inputs(x,y,c) + dCrystalRadG,
-      select(c == CRYSTAL_RAD_B    , inputs(x,y,c) + dCrystalRadB,
+             c == CRYSTAL_RAD_R    , inputs(x,y,c) + dCrystalRad(x,y,0),
+      select(c == CRYSTAL_RAD_G    , inputs(x,y,c) + dCrystalRad(x,y,1),
+      select(c == CRYSTAL_RAD_B    , inputs(x,y,c) + dCrystalRad(x,y,2),
 
-      select(c == DEVEL_CONC       , max(0,inputs(x,y,DEVEL_CONC) -
-                                     dcc*(dCrystalVolR + dCrystalVolG + dCrystalVolB)),
+      select(c == DEVEL_CONC       , max(0,inputs(x,y,c) -
+                                     dcc*(dCrystalVol(x,y,0) + dCrystalVol(x,y,1) + dCrystalVol(x,y,2))),
 
-      select(c == SILVER_SALT_DEN_R, inputs(x,y,c) + sscc*dCrystalVolR,
-      select(c == SILVER_SALT_DEN_G, inputs(x,y,c) + sscc*dCrystalVolG,
-      select(c == SILVER_SALT_DEN_B, inputs(x,y,c) + sscc*dCrystalVolB,
+      select(c == SILVER_SALT_DEN_R, inputs(x,y,c) - sscc*dCrystalVol(x,y,0),
+      select(c == SILVER_SALT_DEN_G, inputs(x,y,c) - sscc*dCrystalVol(x,y,1),
+      select(c == SILVER_SALT_DEN_B, inputs(x,y,c) - sscc*dCrystalVol(x,y,2),
 
       //Otherwise (active crystals) output=input
                                      inputs(x,y,c))))))));
+
+    Var x_outer, x_inner;
+    outputs.split(x,x_outer,x_inner,4).reorder(x_inner,c,x_outer,y)
+             .vectorize(x_inner).parallel(y);
+
+    dCrystalVol.split(x,x_outer,x_inner,4).store_at(outputs,x_outer)
+               .compute_at(outputs,x_outer).vectorize(x_inner);
+
+    dCrystalRad.split(x,x_outer,x_inner,4).store_at(outputs,x_outer)
+               .compute_at(outputs,x_outer).vectorize(x_inner);
 
   return outputs;
 }
@@ -66,11 +73,8 @@ int main(int argc, char **argv) {
     developed = develop(filmulationData, crystalGrowthConst, activeLayerThickness,
                         developerConsumptionConst, silverSaltConsumptionConst,
                         stepTime);
-    Var x_outer, x_inner;
-    developed.split(x,x_outer,x_inner,4).reorder(x_inner,c,x_outer,y)
-             .vectorize(x_inner).parallel(y);
     std::vector<Argument> devArgs = developed.infer_arguments();
     developed.compile_to_file("develop",devArgs);
-    developed.compile_to_lowered_stmt("develop.html");
+    developed.compile_to_lowered_stmt("develop.html",HTML);
     return 0;
 }
