886e39eb5aa0b63f0307a564ac8851d091fb6dce
CarVac <c.lo.to.da.down.lo@gmail.com>
2020-04-28 18:47:56 -0400

    Increase c++ version in CompilerChecks.cmake

--- filmulator-gui/CompilerChecks.cmake
+++ filmulator-gui/CompilerChecks.cmake
@@ -4,7 +4,7 @@ include(CheckCXXCompilerFlagSSP)
 if (UNIX)
     ##################### CXX ########################
 
-    set(CMAKE_CXX_STANDARD 11)
+    set(CMAKE_CXX_STANDARD 14)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
     set(CMAKE_CXX_EXTENSIONS OFF)
 
