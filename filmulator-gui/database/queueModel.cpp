9882246431f42485986f2c7a74fa05e59e5b59f3
CarVac <c.lo.to.da.down.lo@gmail.com>
2020-10-12 20:29:23 -0400

    make right/left jump to image in queue

--- filmulator-gui/database/queueModel.cpp
+++ filmulator-gui/database/queueModel.cpp
@@ -569,3 +569,38 @@ QString QueueModel::getNext(const QString searchID)
         return searchID;
     }
 }
+
+//this can be used to make a mark on the scrollbar
+//it can also be used to keep the active image visible (centered?) on the scrollbar
+float QueueModel::getActivePosition(const QString searchID)
+{
+    if (searchID == "")
+    {
+        return -1;
+    }
+
+    //index 0 out of 0 should be -1 (offscreen)
+    if (maxIndex == 0)
+    {
+        return -1;
+    }
+    //index 0 out of 1 should be 0.5 (centered)
+    if (maxIndex == 1)
+    {
+        return 0;
+    }
+
+    QSqlDatabase db = getDB();
+    QSqlQuery query(db);
+    query.exec("BEGIN TRANSACTION;");
+
+    query.prepare("SELECT QTsortedIndex FROM QueueTable WHERE QTsearchID = ?;");
+    query.bindValue(0, searchID);
+    query.exec();
+    query.next();
+    const int currentIndex = query.value(0).toInt();
+
+    //index 0 out of 2 should be 0 (left)
+    //index 1 out of 2 should be 1 (right)
+    return float(currentIndex)/float(maxIndex - 1);
+}
